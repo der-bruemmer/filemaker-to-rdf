c78e1252c7db3062bf35ebef1218e5c5fb93c828
der.bruemmer <der.bruemmer@localhost>
2012-06-24 15:46:34 +0200

    First version

---
+++ src/de/mpg/eva/valency/ValencyDbObject.java
@@ -0,0 +1,46 @@
+package de.mpg.eva.valency;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValencyDbObject {
+	
+	private int id;	
+	private Map<String, String> fieldValues;
+	
+	public ValencyDbObject() {
+		this.fieldValues = new HashMap<String, String>();
+	}
+	
+	public String getFieldValueByName(String fieldname) {
+		if(this.fieldValues.containsKey(fieldname)) {
+			return this.fieldValues.get(fieldname);
+		} else return null;
+	}
+	
+	public void setFieldValue(String fieldname, String value) {
+		this.fieldValues.put(fieldname, value);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public boolean equals(ValencyDbObject object){
+		return (this.id==object.getId()) ? true : false;
+	}
+	
+	public String toString(){
+		String out=null;
+		if(!this.fieldValues.isEmpty()) {
+			for(String key : this.fieldValues.keySet()) {
+				out+=key + ": " + this.fieldValues.get(key) + " " ;
+			}
+		}
+		return out;
+	}
+}
