c0a0dda825ec697e16132aa53841480eb6bca49d
mrkhfloppy <mrkhfloppy@googlemail.com>
2012-06-25 18:34:35 +0200

    constants for prop file reading

---
+++ src/de/mpg/eva/utils/IConstants.java
@@ -0,0 +1,10 @@
+package de.mpg.eva.utils;
+
+public class IConstants {
+
+	public static final String FM_URL = "filemakerURL";
+	public static final String FM_USER = "filemakerUser";
+	public static final String FM_PASS = "filemakerPassword";
+	public static final String MAPPING_FILE = "filemakerUser";
+
+}
